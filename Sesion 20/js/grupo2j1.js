f59c3174febd467e28836b850ac4f9326491c1e4
Oscar <oscarcm321@gmail.com>
2020-11-05 14:47:13 -0500

    sesion20

---
+++ Sesion 20/js/grupo2j1.js
@@ -0,0 +1,6 @@
+x=2;
+while(x<=40){
+    document.write(x);
+    document.write("\n");
+    x+=2;
+}
---
+++ Sesion 20/js/grupo2j3.js
@@ -0,0 +1,7 @@
+x=2;
+do{
+    document.write(x);
+    document.write("\n");
+    x+=2;
+
+} while(x<=40);
